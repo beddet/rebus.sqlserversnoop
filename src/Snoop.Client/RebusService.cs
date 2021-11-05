fd8828d6492c6a78de6091c1bdf8a6cca9f847cd
Kim Birkelund <kim@birkelund.me>
2021-11-05 10:09:55 +0100

    Upgrade to .NET 6.

--- src/Snoop.Client/RebusService.cs
+++ src/Snoop.Client/RebusService.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Diagnostics;
 using System.Linq;
 using Dapper;
-using Rebus.Messages;
+using Microsoft.Data.SqlClient;
 
 namespace Snoop.Client
 {
